120f1bad79a51bce7cb5ca872f16b6c1607e14eb
AlexLeodanArce <alexcanada201022@gmail.com>
2022-10-23 22:01:05 +0000

    Lab completed

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -1,9 +1,12 @@
 class MonstersController < ApplicationController
   def index
-    @monsters = Monster.all.order(name: :desc)
+    @monsters = Monster.all.order(:id)
   end
 
   def show
     @monster = Monster.find(params[:id])
+    if Tweet.where(monster_id: @monster.id).size < 1
+      redirect_to "/tweets/new", notice: "Cree un tweet"
+    end
   end
 end
